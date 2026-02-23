6a88ae2b4c059d6d681e7d3c5bcc50236f3fdd36
Juan <andevjuan@outlook.com>
2026-02-23 20:11:57 -0300

    SQlite3 implementation for Score using proxy design pattern

--- code/Game.py
+++ code/Game.py
@@ -5,6 +5,7 @@ import pygame
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Level import Level
 from code.Menu import Menu
+from code.Score import Score
 
 
 class Game:
@@ -16,6 +17,7 @@ class Game:
         while True:
             menu = Menu(self.window)
             menu_return = menu.run()
+            score = Score(self.window)
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
                 player_score = [0, 0]
@@ -24,10 +26,16 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
+
             else:
                 pass
 
--- code/Menu.py
+++ code/Menu.py
@@ -24,9 +24,9 @@ class Menu:
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(25, MENU_OPTION[i], C_BLACK, ((WIN_WIDTH / 2), 200 + 20 * i))
+                    self.menu_optext(25, MENU_OPTION[i], C_BLACK, ((WIN_WIDTH / 2), 200 + 20 * i))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], C_GREEN, ((WIN_WIDTH / 2), 200 + 20 * i))
+                    self.menu_optext(20, MENU_OPTION[i], C_GREEN, ((WIN_WIDTH / 2), 200 + 20 * i))
 
             pygame.display.flip()
 
@@ -51,6 +51,12 @@ class Menu:
                         return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name='FoundationOne', size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
+
+    def menu_optext(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name='Sylfaen', size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
